3afee453678ae2ffdf04dfa6b82c15df8467624c
Godwin Ogbara <godwinogbara@Godwins-MacBook-Pro.local>
2023-09-02 23:23:56 -0700

    test

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 document.addEventListener('click', (event) => {
   const target = event.target;
   target.attributeStyleMap.set("background-color", 'black');
+  // Testing commit 
   console.log({event}, target.children)
 } )
